655e9f3716491a3603a62c5387e5704e78b49f7a
clown <yourmama@mail.mail>
2025-07-05 02:07:59 +0300

    to func

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-let uiField = ''; // УИ НА ЧЕКЕ
-let dateField = ''; // АНАЛОГИЧНО
-let formData = new FormData();
-formData.append('orig_date', dateField);
-formData.append('orig_ui', uiField);
-await fetch(`https:\\ch.info-center.by/ajax/check1.php`, {
-    method: 'POST',
-    body: formData
-})
-    .then(response => response.json())
-    .then((data) => {
-        console.log(data.status);
-        // success result
-        if (data.status === 'success') {
-            let arr = JSON.parse(data.message.positions);
-            arr.forEach(i => {
-                console.log(i.product_name + ': ' + i.amount);
-            });
-        }
+async function getReceipt(uiField, dateField) {
+    let formData = new FormData();
+    formData.append('orig_date', dateField);
+    formData.append('orig_ui', uiField);
+    await fetch(`https:\\ch.info-center.by/ajax/check1.php`, {
+        method: 'POST',
+        body: formData
     })
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data.status);
+            return data;
+        })
+}
