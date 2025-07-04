e3e4194a1fb3d8622047d9496ec5800afed8a974
clown <yourmama@mail.mail>
2025-07-05 02:51:43 +0300

    response

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-async function getReceipt(uiField, dateField) {
+async function getReceipt (uiField, dateField) {
     let formData = new FormData();
     formData.append('orig_date', dateField);
     formData.append('orig_ui', uiField);
-    await fetch(`https:\\ch.info-center.by/ajax/check1.php`, {
+    return await(await fetch(`https:\\ch.info-center.by/ajax/check1.php`, {
         method: 'POST',
         body: formData
-    })
-        .then(response => response.json())
-        .then((data) => {
-            console.log(data.status);
-            return data;
-        })
+    })).json();
 }
+
+module.exports = {
+    getReceipt: getReceipt
+};
